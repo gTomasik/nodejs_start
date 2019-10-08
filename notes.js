48ac6fd4a2325179f7df5a2e9999c761a6825f0c
gTomasik <grzesiek315@gmail.com>
2019-10-08 17:03:22 +0200

    adding notes.json

--- notes.js
+++ notes.js
@@ -1,5 +1,34 @@
+const fs = require ('fs')
 const getNotes = () => {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = (title,body) => {
+    const notes = loadNotes()
+    console.log(notes)
+    notes.push ({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes)
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+    
+}
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
